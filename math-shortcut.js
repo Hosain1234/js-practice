1fa05d64803fc252d194d750569d160e730bbd34
hosain1234 <fahim7787@gmail.com>
2022-02-03 20:43:38 +0600

    shortcut added

---
+++ math-shortcut.js
@@ -0,0 +1,38 @@
+var potatoPrice = 89;
+var onionPrice = 34;
+var ricePrice = 10;
+
+
+// akbar kono variable decliar korle pore update er time e var likha lagena sudhu name ta likhe update korlei hoye jay
+potatoPrice = potatoPrice - 50;
+console.log("alur dam: " + potatoPrice);
+
+
+// jodi kisu jog kora lage taile += diye likha jay
+onionPrice += 20;
+// minus er jonno -= use kora lage
+console.log("piaj er dam: " + onionPrice);
+ricePrice -= 5;
+console.log(ricePrice);
+
+
+// gun er jonno *= daoa lage
+ricePrice *= 3;
+console.log(ricePrice);
+
+
+// vag er jonno /= dite hobe
+onionPrice /= 2;
+console.log(onionPrice);
+
+// only 1 barate ++ daoa lage
+var age = 24;
+console.log(age);
+age++;
+console.log(age);
+
+// value 1 kore komate -- use kora lage
+var love = 100;
+console.log(love);
+love --;
+console.log(love);
