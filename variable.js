cfb641c5346d7b2885d5d92f91a6063803577522
hosain1234 <fahim7787@gmail.com>
2022-02-03 17:47:23 +0600

    Variable decliar

--- variable.js
+++ variable.js
@@ -1,1 +1,13 @@
+// declare variable
 var name = "Abdullah al hosain";
+var bananaPrice = 24;
+var isHot = true;
+// console.log(bananaPrice);
+// console.log(name);
+// console.log(isHot);
+
+
+// type of variable
+console.log(typeof name);
+console.log(typeof bananaPrice);
+console.log(typeof isHot);
