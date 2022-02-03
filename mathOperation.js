9303c0f7a5ab2094ac4d1d7d7ce2e83d394d41d8
hosain1234 <fahim7787@gmail.com>
2022-02-03 19:53:51 +0600

    Normal +-*/ done

---
+++ mathOperation.js
@@ -0,0 +1,34 @@
+// simple sum
+
+var price1 = 20;
+var price2 = 23;
+var total = price1 + price2;
+// console.log(total);
+// console.log(price1 + price2);
+
+// simple Substraction 
+var substraction = price2 - price1;
+console.log(substraction);
+console.log(price2 - price1);
+
+// simple multiply
+
+var multiply = price1 * price2;
+console.log(price2 * price1);
+console.log(multiply);
+
+// simple devide
+var devide = price2 / price1;
+console.log(devide);
+console.log(price2 / price1);
+
+// molulas( vag sesh )
+var number1 = 27;
+var number2 = 7;
+var number3 = 9;
+var modulus = number1 % number2;
+var modulus2 = number1 % number3;
+var modulus3 = number2 % number3;
+console.log(modulus);
+console.log(modulus2);
+console.log(modulus3);
