5dd20d306c684d0026da1a30e411e94ea5ed9e32
hosain1234 <fahim7787@gmail.com>
2022-02-03 21:03:19 +0600

    parseInt and parseFloat kore number e convert kora jay kono string ke

---
+++ conversion.js
@@ -0,0 +1,33 @@
+// we can sum two or more string
+var firstName = "Abdullah Al";
+var lastName = 'Hosain';
+var fullName = firstName + lastName;
+// console.log(fullName);
+console.log(firstName + ' ' + lastName);
+
+var onionPrice = '42';
+var eggPrice = 34;
+var bananaPrice = '40';
+// intiger
+var shoe = 2200;
+// float
+var cgpa = 3.05;
+// boolean
+var isCold = true;
+var onionPriceNumber = parseInt(onionPrice);
+// float number re intiger korle . er porer part cancel hoye jay
+var cgpaNumber = parseInt(cgpa);
+// float number er jonno parseFloat korte hoy. tahole . er porer part thake
+var cgpa2 = parseFloat(cgpa);
+console.log(onionPrice);
+console.log(onionPriceNumber);
+console.log(cgpaNumber);
+console.log(cgpa2);
+
+
+
+//  string sum korle pashapashi poshe and number sum korle jogfol dekhay. tai parseInt/parseFloat kore number banaia niye then jog kora lage
+
+
+
+// that's it.
