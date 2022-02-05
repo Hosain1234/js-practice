da392b6bdfa1b79188c1a2bdfaa376067c2329ad
hosain1234 <fahim7787@gmail.com>
2022-02-05 22:08:37 +0600

    for loop done

---
+++ for-loop.js
@@ -0,0 +1,15 @@
+// for loop
+for (i=0; i<10; i++){
+    console.log("Roast den");
+    console.log(i);
+}
+// odd number with for loop
+for(var i=1; i<=10; i+=2){
+    console.log(i);
+}
+// even number using for loop
+for(var i=0; i<=10; i+=2){
+    console.log(i);
+}
+
+// this is basic for loop
