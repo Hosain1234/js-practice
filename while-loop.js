b4f0600f16e6377967feec1a688f2eb89f1dc742
hosain1234 <fahim7787@gmail.com>
2022-02-05 18:46:24 +0600

    regular while loop done

---
+++ while-loop.js
@@ -0,0 +1,6 @@
+// three steps to do 1. decliar variable 2. give condition 3. increase variable/decrease variable
+var rosatGiven = 0;
+while(rosatGiven < 3){
+    console.log("Roast den please!!!");
+    rosatGiven++;
+}
