e133a470146b457b9a955da18778ec01da89eefb
hosain1234 <fahim7787@gmail.com>
2022-02-05 20:06:07 +0600

    while loop done

--- while-loop.js
+++ while-loop.js
@@ -1,6 +1,27 @@
 // three steps to do 1. decliar variable 2. give condition 3. increase variable/decrease variable
 var rosatGiven = 0;
 while(rosatGiven < 3){
-    console.log("Roast den please!!!");
+    // console.log("Roast den please!!!");
     rosatGiven++;
+}
+
+// print 1-10 using while loop
+var startingNumber = 1;
+while(startingNumber <= 10){
+    // console.log(startingNumber);
+    startingNumber++;
+}
+
+// print odd numbers from 1-10
+var beginingNumber = 1;
+while(beginingNumber<=10){
+    // console.log(beginingNumber);
+    beginingNumber = beginingNumber + 2;
+}
+
+// print even numbers from 0-10
+var num1 = 0;
+while(num1<=10){
+    console.log(num1);
+    num1 = num1+2;
 }
